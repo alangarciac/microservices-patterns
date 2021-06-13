0be673351c6aab5ccdd79a50a94f6214331426c6
alancamina <alan.camina@despegar.com>
2021-06-13 13:18:17 -0300

    First commit

---
+++ src/test/java/com/microservicespatterns/MicroservicesPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicespatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
